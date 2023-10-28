2fa1ed1ec806e965480a73b5e6d51fbd2722e3a6
kim2ju <bamtori05@naver.com>
2023-10-28 21:51:32 +0900

    1.3. 컴파일러 최적화

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,39 +6,36 @@ namespace ServerCore
 {
     class Program
     {
-        static void MainThread(object state)
+
+        volatile static bool _stop = false;
+
+        static void ThreadMain()
         {
-            for (int i = 0; i < 5; i++)
-                Console.WriteLine("Hello Thread!");
+            Console.WriteLine("쓰레드 시작!");
+
+            while (_stop == false)
+            {
+
+            }
+
+            Console.WriteLine("쓰레드 종료!");
         }
 
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(5, 5);
-
-            for (int i = 0; i < 5; i++)
-            {
-                Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
-                t.Start();
-            }
-
-/*            for (int i = 0; i < 4; i++)
-                ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });*/
-                
-            ThreadPool.QueueUserWorkItem(MainThread);
-
-/*            Thread t = new Thread(MainThread);
-            t.Name = "Test Thread";
-            t.IsBackground = true;
+            Task t = new Task(ThreadMain);
             t.Start();
 
-            Console.WriteLine("Waiting for Thread!");
+            Thread.Sleep(1000);
 
-            t.Join();
-            Console.WriteLine("Hello World!");*/
-            while (true) { 
-            }
+            _stop = true;
+
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기 중");
+
+            t.Wait();
+
+            Console.WriteLine("종료 성공");
         }
     }
 }
