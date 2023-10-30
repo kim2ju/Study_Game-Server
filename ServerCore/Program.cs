b947d9ead850d1dcc95fa59cbdeddb76d1d886a3
kim2ju <bamtori05@naver.com>
2023-10-30 14:13:28 +0900

    1.8. Deadlock

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,6 +4,48 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    class SessionManager
+    {
+        static object _lock = new object();
+
+        public static void TestSession()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+
+        public static void Test()
+        {
+            lock ( _lock )
+            {
+                UserManager.TestUser();
+            }
+        }
+    }
+
+    class UserManager
+    {
+        static object _lock = new object();
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+
+        public static void TestUser()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+    }
+
     class Program
     {
         static int number = 0;
@@ -11,24 +53,17 @@ namespace ServerCore
 
         static void Thread_1()
         {
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 10000; i++)
             {
-                lock (_obj)
-                {
-                    number++;
-                }
+                SessionManager.Test();
             }
         }
 
         static void Thread_2()
         {
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 10000; i++)
             {
-                Monitor.Enter(_obj);
-
-                number--;
-
-                Monitor.Exit(_obj);
+                UserManager.Test();
             }
         }
 
