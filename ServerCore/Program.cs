3a83a9050aecc82290c93f2275ea73a7219a82ee
kim2ju <bamtori05@naver.com>
2023-11-01 16:27:10 +0900

    1.13. ReaderWriterLock

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,56 +4,39 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-/*    class Lock
-    {
-        AutoResetEvent _available = new AutoResetEvent(true);
-
-        public void Acquire()
-        {
-            _available.WaitOne();
-        }
-
-        public void Release()
-        {
-            _available.Set();
-        }
-    }*/
-
     class Program
     {
 
-        static int _num = 0;
-        static Mutex _lock = new Mutex();
+        static object _lock = new object();
+        static SpinLock _lock2 = new SpinLock();
+        static ReaderWriterLockSlim _lock3 = new ReaderWriterLockSlim();
 
-        static void Thread_1()
+        class Reward
         {
-            for (int i = 0; i < 100000; i++)
-            {
-                _lock.WaitOne();
-                _num++;
-                _lock.ReleaseMutex();
-            }
+
         }
 
-        static void Thread_2()
+        static Reward GetRewardById(int id)
         {
-            for (int i = 0; i < 100000; i++)
-            {
-                _lock.WaitOne();
-                _num--;
-                _lock.ReleaseMutex();
-            }
+            _lock3.EnterReadLock();
+
+            _lock3.ExitReadLock();
+            return null;
         }
+
+        static void AddReward(Reward reward)
+        {
+            _lock3.EnterWriteLock();
+
+            _lock3.ExitWriteLock();
+        }
+
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
-            t1.Start();
-            t2.Start();
+            lock (_lock)
+            {
 
-            Task.WaitAll(t1, t2);
-
-            Console.WriteLine(_num);
+            }
         }
     }
 }
