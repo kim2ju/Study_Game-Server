3aca0627cc6c2ad72f879bbae215a0717313510a
kim2ju <bamtori05@naver.com>
2023-11-01 17:10:37 +0900

    1.14. ReaderWriterLock 구현 연습

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,37 +6,36 @@ namespace ServerCore
 {
     class Program
     {
-
-        static object _lock = new object();
-        static SpinLock _lock2 = new SpinLock();
-        static ReaderWriterLockSlim _lock3 = new ReaderWriterLockSlim();
-
-        class Reward
-        {
-
-        }
-
-        static Reward GetRewardById(int id)
-        {
-            _lock3.EnterReadLock();
-
-            _lock3.ExitReadLock();
-            return null;
-        }
-
-        static void AddReward(Reward reward)
-        {
-            _lock3.EnterWriteLock();
-
-            _lock3.ExitWriteLock();
-        }
+        static volatile int count = 0;
+        static Lock _lock = new Lock();
 
         static void Main(string[] args)
         {
-            lock (_lock)
+            Task t1 = new Task(delegate ()
             {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count++;
+                    _lock.WriteUnlock();
+                }
+            });
+            Task t2 = new Task(delegate ()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count--;
+                    _lock.WriteUnlock();
+                }
+            });
 
-            }
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine(count);
         }
     }
 }
